fb5e703f54c713a28708627d26e1889c8aec67bf
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-07 04:24:32 +0530

    Add upload button event listener for status update

---
+++ js/upload.js
@@ -0,0 +1,9 @@
+const uploadBtn = document.getElementById("uploadBtn");
+
+uploadBtn.addEventListener("click", () => {
+
+document.getElementById("status").innerHTML =
+
+"Preparing secure upload...";
+
+});
